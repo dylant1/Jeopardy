fa13e86de4714a2cda2ce8d3a95d9ad12e4c0aa8
dylant1 <dylant1@yahoo.com>
2022-05-04 14:31:12 -0500

    Updated media queries

--- src/JeopardyGame.js
+++ src/JeopardyGame.js
@@ -140,26 +140,7 @@ const AnswerWrapper = styled.div`
   flex-direction: row;
   align-items: center;
 `;
-function Eye() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="icon icon-tabler icon-tabler-eye"
-      width="20"
-      height="20"
-      viewBox="0 0 24 24"
-      strokeWidth="2.5"
-      stroke="#ff2825"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <circle cx="12" cy="12" r="2" />
-      <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
-    </svg>
-  );
-}
+
 function CheckMark() {
   return (
     <svg
@@ -204,6 +185,26 @@ export const JeopardyGame = () => {
   useEffect(() => {
     progressBar();
   }, [loaded]);
+  function Eye() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-eye eye-icon"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        strokeWidth="2.5"
+        stroke="#ff2825"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <circle cx="12" cy="12" r="2" />
+        <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
+      </svg>
+    );
+  }
   function handleMouseOver(index) {
     const finger = document.getElementById("finger-" + index);
     finger.style.opacity = "1";
